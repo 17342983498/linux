a535b85ef874b91ae88e9f27aee36295421ea7ea
zhuxu <1150559257@qq.com>
2020-11-18 20:05:46 +0800

    虚拟地址

---
+++ linux84/linux-84-0/test1118/Zfork.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+    int ret = fork();
+    if(ret<0)
+    {
+        perror("fork()\n");
+        return -1;
+    }
+    else if(ret==0)
+    {
+        printf("i am child, pid=%d,ppid=%d\n",getpid(),getppid());
+        sleep(1);
+    }
+    else
+    {
+        while(1)
+        {
+            printf("i am father, pid=%d,ppid=%d\n",getpid(),getppid());
+            sleep(1);
+        }
+    }
+    return 0;
+}
