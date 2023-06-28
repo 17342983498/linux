ed52267c5272ad278c41cb87552fa285d3308b3a
17342983498 <1150559257@qq.com>
2023-06-28 19:43:30 +0800

    进程概念(1)

--- linux84/23-6-27/myprocess.c
+++ linux84/23-6-27/myprocess.c
@@ -3,10 +3,15 @@
 #include <sys/types.h>
 int main()
 {
-    while(1)
-    {
-        printf("process running!, pid:%d, ppid:%d\n",getpid(),getppid());
-        sleep(1);
-    }
+  printf("1111111111111111111111111 pid:%d, ppid:%d\n",getpid(),getppid());
+  pid_t ret = fork();
+  printf("2222222222222222222222222 pid:%d, ppid:%d,ret:%d,&ret:%p\n",getpid(),getppid(),ret,&ret);
+
+
+    //while(1) 
+    //{ 
+    //    printf("process running!, pid:%d, ppid:%d\n",getpid(),getppid());
+    //    sleep(1);
+    //}
     return 0;
 }
