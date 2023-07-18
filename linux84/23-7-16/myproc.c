883349cc0cfe2726ad7077707a2e2d6b5ca299ba
17342983498 <1150559257@qq.com>
2023-07-18 19:45:10 +0800

    写时拷贝

--- linux84/23-7-16/myproc.c
+++ linux84/23-7-16/myproc.c
@@ -1,12 +1,44 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <assert.h>
+// int main()
+// {
+//   while(1)
+//   {
+//     printf("hello world!\n");
+//     sleep(1);
+//   }
+//   return 0;
+// }
+
+int num=10;
 
 int main()
 {
-  while(1)
+  int id=fork();
+  assert(id>=0);
+  if(id==0)
   {
-    printf("hello world!\n");
-    sleep(1);
+    //子进程
+    while(1)
+    {
+      printf("i am child,num:%d,&num:%p\n",num,&num);
+      sleep(1);
+    }
+  }
+  else
+  {
+    int cnt=5;
+    //父进程
+    while(1)
+    {
+      printf("i am father,num:%d,&num:%p\n",num,&num);
+      if(--cnt==0)
+      {
+        num=20;
+      }
+      sleep(1);
+    }
   }
   return 0;
 }
